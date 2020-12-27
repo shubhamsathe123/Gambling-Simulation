b4660dfb6953fdc79413622efcfd331391920e74
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-27 14:28:24 +0530

    resigned till 50% stake

--- utility.js
+++ utility.js
@@ -6,6 +6,7 @@ class utility
 {
     gamblingGame(){
         game_result = STAKE_AMOUNT;
+        while((game_result > 50)&&(game_result<150)){
         var game_check = Math.floor( Math.random() * 2 );
         if(game_check == BETTING_AMOUNT){
             game_result = game_result + BETTING_AMOUNT ;
@@ -14,7 +15,11 @@ class utility
             game_result = game_result - BETTING_AMOUNT ;
         }
         console.log(`Game Result ${game_result}`);
+     }
     }
+
+
+    
 }
 
 module.exports=new utility;
