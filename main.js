dc1c80bc4c979dde8ac53e6eea730dd8ee89ad56
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-27 14:23:01 +0530

    bet started

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log("***welcome to gambling simulation***");
 const util=require('./utility.js');
+util.gamblingGame();
--- utility.js
+++ utility.js
@@ -1,9 +1,20 @@
-const STACK_AMOUNT = 100 ;
+const STAKE_AMOUNT = 100 ;
 const BETTING_AMOUNT = 1 ;
+let game_result;
 
 class utility
 {
- 
+    gamblingGame(){
+        game_result = STAKE_AMOUNT;
+        var game_check = Math.floor( Math.random() * 2 );
+        if(game_check == BETTING_AMOUNT){
+            game_result = game_result + BETTING_AMOUNT ;
+        }
+        else{
+            game_result = game_result - BETTING_AMOUNT ;
+        }
+        console.log(`Game Result ${game_result}`);
+    }
 }
 
 module.exports=new utility;
