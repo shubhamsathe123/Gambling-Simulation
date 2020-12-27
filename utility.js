48c7f91a818cc89f4d0d2c4a4d6aa588686ed4f0
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-27 15:03:16 +0530

    calculated monthly stake

--- utility.js
+++ utility.js
@@ -2,6 +2,8 @@ const STAKE_AMOUNT = 100 ;
 const BETTING_AMOUNT = 1 ;
 let game_result;
 const MONTHLY_DAYS=20;
+let monthly_stake = 0;
+let monthly_investment = 0;
 
 class utility
 {
@@ -17,9 +19,29 @@ class utility
               game_result = game_result - BETTING_AMOUNT ;
            }
       }
-      console.log(`Game Result of ${day} day is : ${game_result}`);
-    } 
+      switch(game_result){
+        case ( STAKE_AMOUNT / 2 ) :
+            console.log(`Result of day ${day} is Gambler loss $${game_result}`);
+            break;
+        default :
+            console.log(`Result of day ${day} is Gambler Won $${game_result}`);
+            break;
+      }
+    monthly_investment = monthly_investment + STAKE_AMOUNT;
+    monthly_stake = monthly_stake + game_result;
   }
+    if(monthly_stake > monthly_investment){
+      var total = monthly_stake - monthly_investment ;
+       console.log(`Gambler Won $${total} in this month`)
+     }
+    else{
+       var total =  monthly_investment - monthly_stake ;
+      console.log(`Gambler Loss $${ total } in this month`) 
+     }
+  } 
 }
+
+
+
 
 module.exports=new utility;
